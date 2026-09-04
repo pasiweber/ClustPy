1dbe233495e52617cbcc5fd6149693b694d42d4e
Pascal Weber <pascal.weber@univie.ac.at>
2026-09-04 03:17:10 +0200

    Fix parameter mismatch of handle_noise

--- clustpy/metrics/internal/dcsi.py
+++ clustpy/metrics/internal/dcsi.py
@@ -52,7 +52,7 @@ def dcsi_score(X, labels, min_points=5, noise_strategy="filter"):
     """
 
     assert noise_strategy != "keep", "DCSI score is not defined for noise points."
-    labels, X = handle_noise(labels, strategy=noise_strategy, X=X)
+    labels, X, _ = handle_noise(labels, strategy=noise_strategy, X=X)
     labels = LabelEncoder().fit_transform(labels)
     X, labels = _check_length_data_and_labels(X, labels)
     assert isinstance(labels, np.ndarray), "labels must be of type np.ndarray. Your input has type {0}".format(
