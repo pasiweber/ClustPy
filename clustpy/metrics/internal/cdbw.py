9a0cfd8ea51351d30c155616198fa2d73c558a7e
Pascal Weber <pascal.weber@univie.ac.at>
2026-09-04 03:06:14 +0200

    Fix parameter mismatch of handle_noise

--- clustpy/metrics/internal/cdbw.py
+++ clustpy/metrics/internal/cdbw.py
@@ -507,7 +507,7 @@ def cdbw_score(X, labels, metric="euclidean", noise_strategy="filter", intra_den
         raise ValueError("Parameter s must be > 2")
 
     assert noise_strategy != "keep", "CDbw score is not defined for noise points."
-    labels, X = handle_noise(labels, strategy=noise_strategy, X=X)
+    labels, X, _ = handle_noise(labels, strategy=noise_strategy, X=X)
     labels = LabelEncoder().fit_transform(labels)
     X, labels = _check_length_data_and_labels(X, labels)
     assert isinstance(labels, np.ndarray), "labels must be of type np.ndarray. Your input has type {0}".format(
